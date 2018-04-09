ee77877d6774ced657e9dcaebb724c612411aac0
wojtas500 <37576719+wojtas500@users.noreply.github.com>
2018-04-09 22:06:08 +0200

    Added save functionality

--- src/Actions/Main.java
+++ src/Actions/Main.java
@@ -8,13 +8,43 @@ public class Main {
 	public static double nps = 0;
 	public static double npc = 1;
 	public static int liczbaKlikniec = 0;
-	public static void main(String[] args) {
+package Actions;
+
+import GUI.Gui;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class Main implements Serializable{
+	
+	public static double punkty = 0;
+	public static double nps = 0;
+	public static double npc = 1;
+	public static int liczbaKlikniec = 0;
+	
+	public static void main(String[] args) throws Exception{
 
 		Gui g = new Gui();
 		g.createGui();
 		new PunktyNauki();
 
-
+		Main save = new Main();
+		
+		File f = new File("save.txt");
+		FileOutputStream fos = new FileOutputStream(f);
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(save);
+		
+		FileInputStream fis = new FileInputStream(f);
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		Main load = (Main) ois.readObject();
+		
+		
 	}
+	
 
 }
